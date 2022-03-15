968ed0350094d7cfcc0eaf7585132377b8e60e06
Areko <77594959+WoW-Areko@users.noreply.github.com>
2022-03-15 22:40:53 +0100

    feat: add deDE

--- locales/deDE.lua
+++ locales/deDE.lua
@@ -7,4 +7,11 @@ local ADDONNAME = ...
 local L = LibStub("AceLocale-3.0"):NewLocale(ADDONNAME, "deDE") -- luacheck: ignore 113/LibStub
 if not L then return end
 
+L.OTHER = "Anderes"
+L.MOUNT = _G.MOUNTS
+L.PET = _G.PETS
 
+L.COLLECTED = "|cFF00FF00Eingesammelt|r"
+L.MISSING = "|cFFFF0000Fehltr"
+L.PET_COLLECTED_DD = "|cFF00FF00%d/%d|r"
+L.PET_MISSING_DD = "|cFFFF0000%d/%d|r"
